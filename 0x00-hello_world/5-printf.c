d6ddd34ab97d578b87d09537c28caa7a958d933a
mboyasteps001 <mboyasteps@gmail.com>
2023-02-16 05:50:02 -0800

    a c program that print a line of text using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A Cprogram that print a line using printf function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,\n");
+	return (0);
+}
