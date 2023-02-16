58ce349d5297013df11f50de3f32053deeb20d56
mboyasteps001 <mboyasteps@gmail.com>
2023-02-16 05:22:29 -0800

    wrote ac program that prints the text using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+ * main-entry point
+ *
+ * Description: C program that prints with puts function
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
+}
