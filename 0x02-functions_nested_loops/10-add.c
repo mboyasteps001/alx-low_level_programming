5049704e7c44664ffdb02e3bda9d7686c8a0d888
mboyasteps001 <mboyasteps@gmail.com>
2023-02-21 13:24:08 -0800

    program that return sum of the two input numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that add numbers
+ * @n: first number argument
+ * @m: second input argument
+ * Return: sum of first and second input
+ */
+
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
