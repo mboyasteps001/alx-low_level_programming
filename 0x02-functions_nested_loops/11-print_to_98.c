bd9b9e63659b0fc0374c3e0302303c947dbde968
mboyasteps001 <mboyasteps@gmail.com>
2023-02-21 13:44:58 -0800

    function that returns numberup to 98depending on the input

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that print number to 98
+ *
+ * @n: function input
+ *
+ * Return: the list of numbers
+ */
+
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d", n);
+		if (n != 98)
+			printf(", ");
+		++n;
+	}
+	printf("\n");
+}
