026740af2fd954691a5513539ee1b32b47e4e742
mboyasteps001 <mboyasteps@gmail.com>
2023-02-21 12:20:03 -0800

    some cool changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,15 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; ++j)
 		{
+			_putchar(',');
+			_putchar(' ');
 			product = i * j;
 			if (product < 10)
-			{
-				_putchar(product + '0');
-				_putchar(',');
 				_putchar(' ');
-			}
 			else
 			{
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
