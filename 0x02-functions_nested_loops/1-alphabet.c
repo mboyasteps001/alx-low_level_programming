014522e084c3132e9d0c37121d96c18d26bb405c
mboyasteps001 <mboyasteps@gmail.com>
2023-02-21 06:50:02 -0800

    program manipulate _putchar functionto prints the alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase letter
+ * it also utilize putchar function
+ */
+
+void print_alphabet(void)
+{
+	int chr;
+
+	for (chr = 'a'; chr <= 'z'; ++chr)
+		_putchar(ch);
+	_putchar('\n');
+	
+}
