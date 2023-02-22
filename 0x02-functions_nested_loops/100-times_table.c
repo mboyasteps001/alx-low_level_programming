3788ecd747d486b4b000d542ee541cf9674abae3
mboyasteps001 <mboyasteps@gmail.com>
2023-02-22 00:25:50 -0800

    fix logic

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,10 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= n; ++i)
 		{
-			for (j = 1; j <= n; j++)
+			_putchar('0');
+			for (j = 1; j <= n; ++j)
 			{
 				_putchar(',');
 				_putchar(' ');
