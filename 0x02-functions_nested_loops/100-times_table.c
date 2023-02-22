28329cb162ca10c09a0086593dc754e97468d6dc
mboyasteps001 <mboyasteps@gmail.com>
2023-02-21 23:38:40 -0800

    function that prints table not greater than 15 less than 0

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that print table n times
+ * @n: Function input
+ *
+ * Return: The table
+ */
+
+void print_times_table(int n)
+{
+	int product, i, j;
+
+	if (n < 0 && n > 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				product = i * j;
+				if (product < 10)
+					_putchar(' ');
+				if (product  < 100)
+					_putchar(' ');
+				if (product >= 100)
+				{
+					_putchar((product / 100) + '0');
+					_putchar((product / 10) % 10 + '0');
+				}
+				else if (product <= 99 && product >= 10)
+					_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
