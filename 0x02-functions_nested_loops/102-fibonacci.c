ad8c4368dbf9ecab178f87ed18232105c7d9331b
mboyasteps001 <mboyasteps@gmail.com>
2023-02-22 02:00:29 -0800

    program that prints fibonacci sries of first numbers starting with 1 adn 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: program prints first 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num1 = 1, num2 = 2, fibb, i;
+
+	for (i = 3; i <= 50; i++)
+	{
+		fibb = num1 + num2;
+		printf("%d", fibb);
+		if (i != 50)
+			printf(", ");
+		num1 = num2;
+		num2 = fibb;
+	}
+	printf("\n");
+	return (0);
+}
