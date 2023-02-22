8c6684ebdfb93c65592f30aa3f75cf1432a576f5
mboyasteps001 <mboyasteps@gmail.com>
2023-02-22 03:29:16 -0800

    logic hot fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,16 +10,19 @@
 
 int main(void)
 {
-	long int num1 = 1, num2 = 2, sum = 2, fibb;
+	unsigned long num1 = 1, num2 = 2, fibb;
+	float sum = 2;
 
 	while (num2 <= 40000000)
 	{
 		fibb = num1 + num2;
+		if (sum > 4000000)
+			break;
 		if (fibb % 2 == 0)
 			sum += fibb;
 		num1 = num2;
 		num2 = fibb;
 	}
-	printf("%ld\n", sum);
+	printf("%.0f\n", sum);
 	return (0);
 }
