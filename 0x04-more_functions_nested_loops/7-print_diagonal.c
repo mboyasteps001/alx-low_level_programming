275460555ce770130d55de2bff204c8f6cae814f
mboyasteps001 <mboyasteps@gmail.com>
2023-02-23 07:27:24 -0800

    fixes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -21,7 +21,7 @@ void print_diagonal(int n)
 		{
 			for (space = 0; space < i; ++space)
 				_putchar(' ');
-			_putchar('\\');
+			_putchar(92);	/*ascii value for backlash*/
 			_putchar('\n');
 		}
 	}
