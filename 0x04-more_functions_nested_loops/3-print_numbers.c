e9cf5a1b846c3ea534edccd819bda805ed912992
mboyasteps001 <mboyasteps@gmail.com>
2023-02-23 05:16:14 -0800

    function that prints numbers followed by the newline

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers from 0-9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
