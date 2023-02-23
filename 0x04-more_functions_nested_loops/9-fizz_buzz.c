23ee3619f84867783bafdda73d19d48ed616112c
mboyasteps001 <mboyasteps@gmail.com>
2023-02-23 08:30:43 -0800

    fizzbuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - program that prints specific message when condition us met
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
