4ed4cca949e6344701ded62522253d7d42c4012e
mboyasteps001 <mboyasteps@gmail.com>
2023-02-23 07:22:11 -0800

    function that print backlsh diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - function that print \ nth times
+ *
+ * @n: function input that enter no of times print the diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int i, space;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			for (space = 0; space < i; ++space)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
