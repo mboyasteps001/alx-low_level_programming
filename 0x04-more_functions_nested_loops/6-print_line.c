e30647d30a33dfcb3a50728327e93a4cdc6d6f41
mboyasteps001 <mboyasteps@gmail.com>
2023-02-23 06:19:10 -0800

    logic fixes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,13 @@ void print_line(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < n; ++n)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-			break;
-		}
+
 		_putchar('_');
 	}
 	_putchar('\n');
