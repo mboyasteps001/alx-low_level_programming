94ee30630642e712175af3f935cd9b7eaa4136ba
mboyasteps001 <mboyasteps@gmail.com>
2023-02-27 14:30:32 -0800

    changes and fix logic

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -23,7 +23,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = ((counter - 1) / 2) + 1; str[i] != '\0'; ++i)
+		for (i = (counter - 1) / 2; str[i] != '\0'; ++i)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
