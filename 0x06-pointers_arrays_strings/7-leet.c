78a2f0facd6884592bb394604255326540863069
mboyasteps001 <mboyasteps@gmail.com>
2023-03-02 06:14:12 -0800

    Function that substitute the character with respective numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - The function that converts the numbers
+ * into their respective given number
+ * @str:Input string
+ * Return:Converted string
+ */
+
+char *leet(char *str)
+{
+	int i;
+	char *result = str;
+
+	/*Loop each character*/
+	for ( ; *str != '\0'; str++)
+	{
+		for (i = 0; i < 5; ++i)
+		{
+			if (*str == "aAeEoOtTlL"[i])
+			{
+				*str = "43071"[i];
+				break;
+			}
+		}
+	}
+	return (result);
+}
