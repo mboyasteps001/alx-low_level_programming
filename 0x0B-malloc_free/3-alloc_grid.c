e4532d6eb20787c9b8ab8a47122b806d9a950323
mboyasteps001 <mboyasteps@gmail.com>
2023-03-14 09:44:52 -0700

    Function that returns 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Function that returns 2D array to located memory
+ * @width:The width of the array(columns)
+ * @height:Heiht of array(rows)
+ * Return: The 2D array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int));
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; ++i)
+	{
+		grid[i] = calloc(width, sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
