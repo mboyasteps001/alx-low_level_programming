01497829a0319401620cbabb7a8104902bacc1e6
mboyasteps001 <mboyasteps@gmail.com>
2023-03-14 10:10:59 -0700

    another logic fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,16 +20,18 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; ++i)
 	{
-		grid[i] = calloc(width, sizeof(int));
+		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
+			for (; i >= 0; --i)
 			{
 				free(grid[j]);
 			}
 			free(grid);
 			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 	return (grid);
 }
