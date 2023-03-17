72d578342c548982f78b4cdfa06ded574f4e2218
mboyasteps001 <mboyasteps@gmail.com>
2023-03-17 07:10:43 -0700

    Function that check for correct assignemnt of memory to the variable

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+
+#endif
