8d86a26e558ce1ca9d54ff5410126d1e72cb25ba
mboyasteps001 <mboyasteps@gmail.com>
2023-03-22 00:54:35 +0300

    Header file with the structure dog which initializes the name, age and the dog owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - dog structure
+  *@name:Name of the dog
+  *@age:The age of the dog
+  *@owner:The ownner of the dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
