4a4269d2763859a9020103742242a4c4049fa4c0
mboyasteps001 <mboyasteps@gmail.com>
2023-03-24 00:32:44 +0300

    Function that pass the functions as arguments

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+  *array_iterator - Function that executes a function as a given parameter
+  *@array:Function input
+  *@size: The size of the array
+  *@action: Pointer to function
+  *Return: Nothing
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
