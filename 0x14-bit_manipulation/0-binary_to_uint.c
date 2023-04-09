77d6b47f664d97c81b2427fcbf471711d7d5e7f5
mboyasteps001 <mboyasteps@gmail.com>
2023-04-09 08:21:45 +0300

    Function converts binary number into decimal number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+  *_strlen - Funciton that dertemine the lenght of the string
+  *@str: Pointer to the string to be vonverted
+  *Return: Number of string
+  */
+
+unsigned int _strlen(const char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+  *_atoi - Function that converts the string into integer
+  *@c: Pointer to string to be converted
+  *Return: Unsigned int
+  */
+
+unsigned int _atoi(const char c)
+{
+	return ((unsigned int)(c - '0'));
+
+}
+
+/**
   *binary_to_uint - Function that converts binary into decimal number
   *@b: Input string with binary numbers
   *Return: unsigned int
@@ -8,23 +35,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0, power;
-	int len = 0, i;
-
+	unsigned int decimal = 0;
+	
 	if (b == NULL)
 		return (0);
-	while (b[len] != '\0')
-	{
-		if (b[len] != '0' || b[len] != '1')
-			return (0);
-		len++;
-	}
 
-	for (i = len - 1; i >= 0; i--)
+	while(*b != '\0')
 	{
-		if (b[i] == '1')
-			decimal += power;
-		power *= 2;
+		if (*b != '0' || *b != '1')
+			return (0);
+		else
+		{
+			decimal = decimal * 2 + (*b - '0');
+			b++;
+		}
 	}
 	return (decimal);
 }
