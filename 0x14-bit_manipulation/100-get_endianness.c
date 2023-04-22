41bde6baa4fe244c3a94e4647440ca1eb37e8524
mboyasteps001 <mboyasteps@gmail.com>
2023-04-22 22:30:23 +0300

    Fix betty errors

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  *get_endiannes - Function that check for endianness
+  *get_endianness - Function that check for endianness
   *Return: 0 if big endian or 1 for little endian
   */
 
@@ -9,7 +9,7 @@ int get_endianness(void)
 {
 	int i = 1;
 	char *p = (char *) &i;
-	
+
 	if (p[0] == 1)
 		return (1);
 	else
