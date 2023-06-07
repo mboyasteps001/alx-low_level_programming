d46039672507642a92ead6ead54e0c65aeb31caf
mboyasteps001 <mboyasteps@gmail.com>
2023-06-07 17:15:50 +0300

    Function that calculates the power of the number using recursive calls

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - FUnction that returns the power of the number
+ * @x: Interger input
+ * @y: Integer that indicates the pwer of the number
+ * Return: Power of the number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int answer = x;
+
+	if (y < 0)
+		return (-1);
+	if (y ==0)
+		return (1);
+	answer *= _pow_recursion(x, y - 1);
+	return (answer);
+
+}
