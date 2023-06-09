43d6061f7a4de0cce89585072a39a30d432c4c15
mboyasteps001 <mboyasteps@gmail.com>
2023-06-09 18:24:06 +0300

    Created a library named libmy.a from the required files

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenate strings
+ * @dest:first function argument
+ * @src:second function argument
+ * Return: The string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c = 0, cc = 0;
+
+	while (dest[c])
+		c++;
+	while (src[cc])
+	{
+		dest[c++] = src[cc];
+		++cc;
+	}
+	return (dest);
+}
